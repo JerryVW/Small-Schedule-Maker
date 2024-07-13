4e064662b21262c7f4f9718cc741170ace31ca05
JerryVW <jerryvw72@gmail.com>
2024-07-13 10:09:29 -0400

    Set up html and css for javacript project

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+let weekday = document.getElementById('weekday').value;
+
+const getWeekday = function (weekday) {
+	console.log(weekday.innerHTML);
+	return weekday.innerHTML;
+};
+
+getWeekday(weekday);
